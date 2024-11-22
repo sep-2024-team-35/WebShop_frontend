56a4527faece6d7ee4b2e09824327f7b32419911
Ninoli1 <nikolinaskiljevic@gmail.com>
2024-11-22 14:30:06 +0100

    small route fix

--- WebShop_frontend/src/app/app.routes.ts
+++ WebShop_frontend/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './layout/home/home.component';
 import { ShowServicesPackagesComponent } from './servicesAndPackages/show-services-packages/show-services-packages.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+    { path: '', redirectTo: '/login', pathMatch: 'full' }, 
     {path:'login',component:LoginComponent},
     {path:'register',component:RegisterComponent},
     {path:'home',component:HomeComponent},
